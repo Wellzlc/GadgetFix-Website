a7a313bc7b8e369e580db7d08b116168f2948346
Wellzlc <136069527+Wellzlc@users.noreply.github.com>
2025-08-10 02:09:26 -0500

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,22 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    inlineStylesheets: 'auto',
+    split: true
+  },
+  compressHTML: true,
+  vite: {
+    build: {
+      cssCodeSplit: true,
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      }
+    }
+  }
+});
